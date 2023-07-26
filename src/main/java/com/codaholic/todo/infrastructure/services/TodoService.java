6b165b8e9cd25fd528322e6f47892eb1001bb06e
Emmy Steven <hello@emmysteven.com>
2023-07-27 00:15:36 +0100

    create service for todo

---
+++ src/main/java/com/codaholic/todo/infrastructure/services/TodoService.java
@@ -0,0 +1,48 @@
+package com.codaholic.todo.infrastructure.services;
+
+import com.codaholic.todo.domain.entities.Todo;
+import com.codaholic.todo.infrastructure.repository.TodoRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class TodoService {
+    private final TodoRepository repo;
+
+    TodoService(TodoRepository repo){
+        this.repo = repo;
+    }
+
+
+    public List<Todo> getAllTodos() {
+        return repo.findAll();
+    }
+
+    public Todo createTodo(Todo todo) {
+        return repo.save(todo);
+    }
+
+    public Todo getTodoById(UUID id) {
+        return repo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Todo not found"));
+    }
+
+    public Todo updateTodo(UUID id, Todo todoDetail) {
+        Todo todo = repo.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Todo not found"));
+
+        todo.setName(todoDetail.getName());
+        todo.setStartTime(todoDetail.getStartTime());
+        todo.setEndTime(todoDetail.getEndTime());
+        todo.setTaskStatus(todoDetail.getTaskStatus());
+
+        return repo.save(todo);
+    }
+
+    public void deleteTodo(UUID id) {
+        repo.deleteById(id);
+    }
+}
+
