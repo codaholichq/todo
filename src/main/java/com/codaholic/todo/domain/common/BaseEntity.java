fb0f2ff521f1771f530bb8007c0ea62a56a10720
Emmy Steven <hello@emmysteven.com>
2023-07-26 23:39:01 +0100

    add a base entity

---
+++ src/main/java/com/codaholic/todo/domain/common/BaseEntity.java
@@ -0,0 +1,40 @@
+package com.codaholic.todo.domain.common;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+
+@Data
+@MappedSuperclass
+public abstract class BaseEntity implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 5L;
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @JsonIgnore
+    @CreatedDate
+    @Column(name = "created_dt")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime createdDate;
+
+    @JsonIgnore
+    @LastModifiedDate
+    @Column(name = "updated_dt")
+    @Temporal(TemporalType.TIMESTAMP)
+    private LocalDateTime updatedDate;
+
+    @JsonIgnore
+    @Version
+    private Integer version;
+}
